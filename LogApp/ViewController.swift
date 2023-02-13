c4c111b5d96e3fb61daafac98cf5a2fbce5a11d5
Kirill <akulenko818@gmail.com>
2023-02-13 08:19:16 +0300

    Initial Commit

---
+++ LogApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LogApp
+//
+//  Created by user on 13.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
